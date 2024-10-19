beebb8cbbf7e0b3819db54ddca614f74575a0e3b
Alejandra García <168131275+AleG3921@users.noreply.github.com>
2024-10-19 01:22:21 -0600

    Creación de la Clase "TemperatureConverterTest"

---
+++ MAGJ.Temperatura.UnitTest.AppMSTest/TemperatureConverterTest.cs
@@ -0,0 +1,33 @@
+﻿using MAGJ.Temperatura;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MAGJ.Temperatura.UnitTest.AppMSTest
+{
+    [TestClass]
+    public class TemperatureConverterTest
+    {
+        [TestMethod]
+        public void CelsiusToFahrenheit()
+        {
+           TemperatureConverter converter = new TemperatureConverter();
+
+            double temperature = converter.CelsiusToFahrenheit(88);
+
+            Assert.AreEqual(0.0, temperature);
+
+        }
+        public void FahrenheitToCelsius()
+        {
+            TemperatureConverter converter = new TemperatureConverter();
+
+            double temperature = converter.CelsiusToFahrenheit(100);
+
+            Assert.AreEqual(0.0, temperature);
+
+        }
+    }
+}
